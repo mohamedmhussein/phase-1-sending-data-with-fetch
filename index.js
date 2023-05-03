566d61c81c3eedf51ac7088a04299975fefd0f58
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-05-02 21:22:46 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users',{
+    method: 'POST',    
+    headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+                })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector('body').innerHTML = data.id
+    })
+    .catch(function (error) {
+        const message = 'Unauthorized Access'
+        //alert("Unauthorized Access");
+        const p = document.createElement('p')
+        p.textContent = message
+        document.querySelector('body').appendChild(p)
+      });
+}
